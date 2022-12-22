f29eac3c0747729296cf707237ff89052be16acd
jaccobezemer <jaccobezemer@users.noreply.github.com>
2022-12-22 08:27:01 +0100

    comments updated

--- src/main.cpp
+++ src/main.cpp
@@ -10,8 +10,8 @@
 #define P3_SERIAL_TX  1   // P3 -> UART0 which is attached to Alfen Loadbalancer RX pin 
 #define P3_DR         35  // P3 Data request
 #define P1_LED        14  // lights up when a telegram is received and stored
-#define P2_LED        27  // lights up when a P2 requests data
-#define P3_LED        26  // lights up when a P3 requests data
+#define P2_LED        27  // lights up when P2 requests data
+#define P3_LED        26  // lights up when P3 requests data
 
 void disableWiFi();
 
@@ -41,9 +41,9 @@ struct Printer {
 P1Reader reader(&Serial1,P1_DR);
 
 void setup() {
-  disableWiFi(); // we don't need WiFi so disable it to save power so this device can be powered by the DSMR meter (if other phantom powered peripherals are not too power demanding. Be aware, all must be under 250mA)
+  disableWiFi(); // WiFi is not needed so disable it and draw less power
   //Serial.begin(115200);                                   //  UART0, now used for monitoring, later for Alfen Load Balancer
-  //Serial.println("DSMR P1 Smart Splitter, Copyright 2022, Jacco Bezemer");
+  //Serial.println("Smart Splitter for DSMR P1, Copyright 2022, Jacco Bezemer");
   pinMode(P2_DR,INPUT); // or should this be INPUT_PULLUP
   pinMode(P3_DR,INPUT); // or should this be INPUT_PULLUP
   pinMode(P1_LED,OUTPUT_OPEN_DRAIN);
@@ -52,38 +52,39 @@ void setup() {
   digitalWrite(P1_LED,HIGH);
   digitalWrite(P2_LED,HIGH);
   digitalWrite(P3_LED,HIGH);
-  Serial1.begin(115200,SERIAL_8N1,P1_SERIAL_RX,-1,false);  //  Telgram receive port, UART1 attached to DSMR smartmeter receiving telegrams, no send pin nessesary
-  Serial2.begin(115200,SERIAL_8N1,-1,P2_SERIAL_TX,false);  //  UART2 attached to HomeWizard sending telegrams, no receive pin nessesary
-  Serial.begin(115200,SERIAL_8N1,-1,P3_SERIAL_TX,false);   //  UART0 attached to Alfen Load Balancer sending telegrams, no receive pin nessesary
-  reader.enable(false);                                   //  Reads the telegram send by the P1 meter 
+  Serial1.begin(115200,SERIAL_8N1,P1_SERIAL_RX,-1,false);  //  Telgram receive port, UART1 attached to DSMR P1 port receiving telegrams at maximum interval, no send pin nessesary
+  Serial2.begin(115200,SERIAL_8N1,-1,P2_SERIAL_TX,false);  //  UART2 attached to HomeWizard send telegrams, on request, no receive pin nessesary
+  Serial.begin(115200,SERIAL_8N1,-1,P3_SERIAL_TX,false);   //  UART0 attached to Alfen Load Balancer to send telegrams on request, no receive pin nessesary
+  reader.enable(false);                                    //  Reads the telegram send by the P1 meter 
 }
 
 void loop() {
-  // Allow the reader to check the serial buffer regularly
-  reader.loop();
+  
+  reader.loop();                      // Allow the reader to check the serial buffer regularly
 
-  if (reader.available()) {
-    digitalWrite(P1_LED,LOW);
+  if (reader.available()) {           // An complete and correct telegram has been received
+    digitalWrite(P1_LED,LOW);         // turn on the telegram stored led
 
     // send the telegram to HomeWizard P1 meter
     if (digitalRead(P2_DR) == LOW) {
-      digitalWrite(P2_LED,LOW);
-      Serial2.print("/"); // -> is stripped from the raw message
-      Serial2.print(reader.raw());
-      Serial2.print(reader.rawCRC()); // -> is stripped from the raw message
-      digitalWrite(P2_LED,HIGH);  // is this long enough to see the led blink?
+      digitalWrite(P2_LED,LOW);       // turn on the data request led for P2
+      Serial2.print("/");             // this whas stripped from the stored telegram so it need's to be send ahead
+      Serial2.print(reader.raw());    // sends the telegram that whas stored by the reader
+      Serial2.print(reader.rawCRC()); // this whas stripped from the stored telegram so it need't to be send after
+      digitalWrite(P2_LED,HIGH);      // is this long enough to see the led blink?
     }
 
     // send the telegram to Alfen Load Balancer
     if (digitalRead(P3_DR) == LOW) {
-      digitalWrite(P3_LED,LOW);
-      Serial.print("/"); // -> is stripped from the raw message
-      Serial.print(reader.raw());
-      Serial.print(reader.rawCRC()); // -> is stripped from the raw message
-      digitalWrite(P3_LED,HIGH);  // is this long enough to see the led blink?
+      digitalWrite(P3_LED,LOW);       // turn on the data request led for P3
+      Serial.print("/");              // this whas stripped from the stored telegram so it need's to be send ahead
+      Serial.print(reader.raw());     // sends the telegram that whas stored by the reader
+      Serial.print(reader.rawCRC());  // this whas stripped from the stored telegram so it need't to be send after
+      digitalWrite(P3_LED,HIGH);      // is this long enough to see the led blink?
     }
 
-    reader.clear();
+    reader.clear();                   // clears the stored telegram
+    
     // decode the data so it can be presented on a display (when attached, for now print to serial monitoring)
     /*
     MyData data;
@@ -96,7 +97,8 @@ void loop() {
       Serial.println(err);
     }
     */
-    digitalWrite(P1_LED,HIGH); // is this long enough to see the led blink? Yes, it is.
+    
+    digitalWrite(P1_LED,HIGH);        // turn off the telegram stored led
   }  
 
 }
